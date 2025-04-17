d67092c2c33c4480e32a32e7e198ed0f450b6012
thibMC <thibaut.marin-cudraz@reef-pulse.com>
2025-04-17 17:25:35 +0400

    test lago VGG19

--- calibrage_sous_espace_VGGish.py
+++ calibrage_sous_espace_VGGish.py
@@ -30,6 +30,7 @@ from sklearn.cluster import (
 )
 import keras
 from keras.applications.vgg19 import preprocess_input
+from multiprocessing import Pool
 
 ### Variables ne dépendant pas de la fréquence d'échantillonnage
 f_filt = [950, 2800]  # bande de fréquences pour la filtration
@@ -39,11 +40,11 @@ wlen_env = 2048  # taille de fenete pour spectro imprecis
 ovlp = 0.75  # overlap spectro
 ovlp_env = 0.9  # overlap spectro enveloppe
 max_dur = 4  # durée en secondes maximale d'un son
-wd = "CCFlaine2017"  # répertoire contenant les sons
-graine = 20250218  # graine pour UMAP
-n_features = np.arange(10, 110, 10)
-n_features = np.arange(52, 68, 2)
-n_features = [54, 55, 56]
+wd = "/home/tmc/Documents/LPO_Prestation/Analyses/CCFlaine2017"  # répertoire contenant les sons
+# graine = 20250218  # graine pour UMAP
+# n_features = np.arange(10, 110, 10)
+# n_features = np.arange(52, 68, 2)
+# n_features = [54, 55, 56]
 
 
 # ### Load model
@@ -174,5 +175,115 @@ def features_from_spec(
         include_top=False, weights="imagenet", input_tensor=new_input, pooling="avg"
     )
     prediction = model.predict(imagette)
-
     return prediction.ravel()
+
+
+### Test et mesure du temps d'execution
+# Recuperation des sons filtrés
+t1 = time.time()
+noms, signaux, freq_ech = filtre_sons(wd)
+t2 = time.time()
+print("Filtration : " + str(t2 - t1))  # 21.33 secs
+# Extractions des features
+t1 = time.time()
+features = []
+for w in wlen:
+    f = [
+        features_from_spec(
+            signaux[k], w, ovlp, wlen_env, ovlp_env, freq_ech[k], max_dur, f_filt
+        )
+        for k in range(len(signaux))
+    ]
+    f = np.dstack(f)
+    f = np.squeeze(f, 0)
+    features.append(f.T)
+
+t2 = time.time()
+print("Features : " + str(t2 - t1))  # 12.36 secs
+
+
+# Récuperation des individus via le nom de fichier
+indiv = np.array([n.split("_")[1] for n in noms])
+
+
+def cluster_features(array_features, nom_males=indiv, n_iter=100):
+    n_sons = array_features.shape[0]
+    n_clust = np.arange(2, n_sons)
+    cluster_number_found = np.zeros((n_iter, 6)).astype(int)
+    rand_index = np.zeros((n_iter, 6))
+    for i in range(n_iter):
+        sil = np.zeros((4, len(n_clust)))
+        for c in n_clust:
+            agglo_clust = AgglomerativeClustering(n_clusters=c).fit_predict(
+                array_features
+            )
+            gmm_clust = GaussianMixture(n_components=c).fit_predict(array_features)
+            km_clust = KMeans(n_clusters=c).fit_predict(array_features)
+            bkm_clust = BisectingKMeans(n_clusters=c).fit_predict(array_features)
+            sil[:, c - 2] = [
+                m.silhouette_score(array_features, clust)
+                for clust in [agglo_clust, gmm_clust, km_clust, bkm_clust]
+            ]
+        cluster_number_found[i, : sil.shape[0]] = [
+            n_clust[np.argmax(sil[i_s, :])] for i_s in range(sil.shape[0])
+        ]
+        hdbscan_clust = HDBSCAN(min_cluster_size=2).fit_predict(array_features)
+        cluster_number_found[i : sil.shape[0]] = len(
+            np.unique(hdbscan_clust[hdbscan_clust != -1])
+        )
+        ms_clust = MeanShift(n_jobs=-1).fit_predict(array_features)
+        cluster_number_found[i : sil.shape[0] + 1] = max(ms_clust) + 1
+        rand_index[i] = [
+            m.rand_score(nom_males, clustering_best)
+            for clustering_best in [
+                AgglomerativeClustering(
+                    n_clusters=cluster_number_found[i, 0]
+                ).fit_predict(array_features),
+                GaussianMixture(n_components=cluster_number_found[i, 1]).fit_predict(
+                    array_features
+                ),
+                KMeans(n_clusters=cluster_number_found[i, 2]).fit_predict(
+                    array_features
+                ),
+                BisectingKMeans(n_clusters=cluster_number_found[i, 3]).fit_predict(
+                    array_features
+                ),
+                hdbscan_clust,
+                ms_clust,
+            ]
+        ]
+    cluster_number_found = pd.DataFrame(
+        cluster_number_found,
+        columns=[
+            "agglomerative",
+            "gmm",
+            "kmeans",
+            "bisecting_kmeans",
+            "hdbscan",
+            "meanshift",
+        ],
+    )
+    rand_index = pd.DataFrame(
+        rand_index,
+        columns=[
+            "agglomerative",
+            "gmm",
+            "kmeans",
+            "bisecting_kmeans",
+            "hdbscan",
+            "meanshift",
+        ],
+    )
+    return (cluster_number_found, rand_index)
+
+
+# pool = Pool()
+# t1 = time.time()
+# clustering_diagnostique = pool.map(cluster_features, features)
+# t2 = time.time()
+# print("Multiprocessing: " + str(t2 - t1))
+# pool.close()
+# pool.join()
+
+
+clustering_diagnostique = [cluster_features(f) for f in features]
